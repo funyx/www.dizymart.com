9d25aa1c0ba9fb8f6982b92abe305f546d618755
Dimitar Dobchev <ddobchev@gmail.com>
2015-11-08 20:51:23 +0200

    asd

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -1,8 +1,8 @@
 module.exports = function (grunt) {
 	grunt.registerTask('prod', [
 		'compileAssets',
-		'concat',
-		'uglify',
+		//'concat',
+		//'uglify',
 		'cssmin',
 		'sails-linker:prodJs',
 		'sails-linker:prodStyles',
