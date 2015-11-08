aa263ba673eaac8a18c0e71615a1c06fd059f6c1
Dimitar Dobchev <ddobchev@gmail.com>
2015-11-08 21:12:18 +0200

    1

--- assets/src/app/home/index.js
+++ assets/src/app/home/index.js
@@ -1,22 +1,19 @@
-angular.module( 'sailng.home', ['ui.router',
-'sails.io',
-'angularMoment',
-'lodash'
-])
+angular.module( 'sailng.home', ['ui.router'])
 .config(function config( $stateProvider ) {
 	'use strict';
-	$stateProvider.state( 'home', {
-		url: '/home',
-		views: {
-			"main": {
-				controller: 'HomeCtrl',
-				templateUrl: 'home/index.tpl.html'
-			}
-		}
-	});
+	// $stateProvider.state( 'home', {
+	// 	url: '/home',
+	// 	views: {
+	// 		"main": {
+	// 			controller: 'HomeCtrl',
+	// 			templateUrl: 'home/index.tpl.html'
+	// 		}
+	// 	}
+	// });
 })
 
-.controller( 'HomeCtrl', function HomeController( $scope, titleService ) {
+.controller( 'HomeCtrl', function HomeController(){// $scope, titleService ) {
 	'use strict';
-	titleService.setTitle('Home');
+	console.log('opa');
+	//titleService.setTitle('Home');
 });
