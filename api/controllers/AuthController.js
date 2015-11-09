c27c583926fc006ff7f648096779b70a6c2d0e6a
Dimitar Dobchev <ddobchev@gmail.com>
2015-11-09 13:22:29 +0200

    added models and controller

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,51 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing auths
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `AuthController.login()`
+   */
+  login: function (req, res) {
+    return res.json({
+      todo: 'login() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `AuthController.register()`
+   */
+  register: function (req, res) {
+    return res.json({
+      todo: 'register() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `AuthController.forgot_password()`
+   */
+  forgot_password: function (req, res) {
+    return res.json({
+      todo: 'forgot_password() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `AuthController.provider()`
+   */
+  provider: function (req, res) {
+    return res.json({
+      todo: 'provider() is not implemented yet!'
+    });
+  }
+};
+
